71f2c4ca3271f3f73082d340b9ed90ecbd735b7a
andreathepa <andreadepa32@gmail.com>
2023-05-22 17:49:19 +0200

    ciclo for + email matched

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
-//Creare uno spazio in cui l'utente possa scrivere il proprio nome
+//Creare uno spazio in cui l'utente possa scrivere la propria email
 
-// fare in modo che ciò che ha scritto l'utente venga riconosciuto 
+// fare in modo che ciò che ha scritto l'utente venga riconosciuto tra le email disponibili che sono: 
+// tizio@gmail.com
+// caio@gmail.com  
+// semprogno@gmail.com
+// vattelapesca@gmail.com
+
 
 // elaborare il messaggio
-
-let email = prompt("Per favore inserisci la tua email")
-
-console.log(email)
-
 let namemail = ['tizio@gmail.com', 'caio@gmail.com', 'semprogno@gmail.com', 'vattelapesca@gmail.com'];
 
-console.log(namemail);
+
+let email = prompt("Per favore inserisci la tua email");
+
+console.log(email);
+
+let message = '';
 
 let flag = false;
+
+for(let i=0; i<namemail.length; i++){
+
+    if(namemail [i].toLowerCase() === email.toLowerCase()){
+        flag = true;
+    }
+}
+
+if(flag === true){
+    console.log("Complimenti l'email è presente nei nostri sistemi");
+    message = "Complimenti l'email è presente nei nostri sistemi"
+}
+
+else{
+    console.log("Spiacenti l'email inserita non è presente nei nostri sistemi");
+}
